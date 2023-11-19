d9ba8eadabfb1950a3f4596f4dfb7bbc470ee6e4
Howard Zhang <zamhown@live.com>
2023-11-19 22:38:56 +0800

    feat: responsive theme switch

--- packages/vchart-semi-theme/__tests__/runtime/browser/src/index.jsx
+++ packages/vchart-semi-theme/__tests__/runtime/browser/src/index.jsx
@@ -6,6 +6,23 @@ import { initVChartSemiTheme } from '../../../../src';
 
 initVChartSemiTheme();
 
+// 匹配系统亮暗模式
+const mql = window.matchMedia('(prefers-color-scheme: dark)');
+function matchMode(e) {
+  const body = document.body;
+  if (e.matches) {
+    if (!body.hasAttribute('theme-mode')) {
+      body.setAttribute('theme-mode', 'dark');
+    }
+  } else {
+    if (body.hasAttribute('theme-mode')) {
+      body.removeAttribute('theme-mode');
+    }
+  }
+}
+matchMode(mql);
+mql.addListener(matchMode);
+
 const dom = document.querySelector('#root');
 const root = createRoot(dom);
 root.render(<Frame />);
--- packages/vchart-semi-theme/__tests__/runtime/browser/vite.config.js
+++ packages/vchart-semi-theme/__tests__/runtime/browser/vite.config.js
@@ -1,5 +1,3 @@
-import * as path from 'path';
-import localConf from './vite.config.local';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
@@ -7,7 +5,6 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: localConf.port || 3000,
     https: !!process.env.HTTPS,
     open: true
   },
@@ -15,12 +12,5 @@ export default defineConfig({
     __DEV__: true,
     __VERSION__: JSON.stringify(require('../../../package.json').version)
   },
-  plugins: [react(), svgr()],
-  resolve: {
-    ...localConf?.resolve,
-    alias: {
-      ...localConf?.resolve?.alias
-      //'@visactor/vutils-extension': path.resolve(__dirname, '../../../../vutils-extension/src/index.ts')
-    }
-  }
+  plugins: [react(), svgr()]
 });

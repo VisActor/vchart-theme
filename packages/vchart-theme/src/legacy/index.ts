f577f931661d8a0feeb296cf717b894cb13ad3ee
Howard Zhang <zamhown@live.com>
2024-01-11 00:10:31 +0800

    feat: add legacy light and dark theme

--- packages/vchart-theme/src/legacy/index.ts
+++ packages/vchart-theme/src/legacy/index.ts
@@ -2,9 +2,20 @@ import { colorLegend } from './legend/color-legend';
 import { sizeLegend } from './legend/size-legend';
 import type { ITheme } from '@visactor/vchart-types';
 
-export const legacyTheme: ITheme = {
-  name: 'legacy',
-  description: 'legacy theme for simply legend style',
+export const legacyLightTheme: ITheme = {
+  name: 'legacyLight',
+  description: 'legacy light theme for simply legend style',
+  type: 'light',
+  component: {
+    sizeLegend,
+    colorLegend
+  }
+};
+
+export const legacyDarkTheme: ITheme = {
+  name: 'legacyDark',
+  description: 'legacy dark theme for simply legend style',
+  type: 'dark',
   component: {
     sizeLegend,
     colorLegend
--- packages/vchart-theme/src/theme-map.ts
+++ packages/vchart-theme/src/theme-map.ts
@@ -1,11 +1,12 @@
 import type { ITheme } from '@visactor/vchart-types';
 import { vScreenThemeList } from './v-screen';
-import { legacyTheme } from './legacy';
+import { legacyDarkTheme, legacyLightTheme } from './legacy';
 
 export const allThemeMap = new Map([
   // 大屏主题
   ...([...vScreenThemeList].map(theme => [theme.name, theme]) as [string, ITheme][]),
   // 传统主题
-  [legacyTheme.name, legacyTheme]
+  [legacyLightTheme.name, legacyLightTheme],
+  [legacyDarkTheme.name, legacyDarkTheme]
   // new theme here
 ]) as Map<string, ITheme>;

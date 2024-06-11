03ac0989e7651ac059be23a1933912672313aa15
xiaoluoHe <sevnape@outlook.com>
2024-06-11 11:52:18 +0800

    chore: use css instead of less

--- packages/vchart-theme-demo-component/src/vchart-chart-hub-theme/frame.tsx
+++ packages/vchart-theme-demo-component/src/vchart-chart-hub-theme/frame.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Nav, Avatar } from '@douyinfe/semi-ui';
 import { IconFeishuLogo, IconHelpCircle, IconBell } from '@douyinfe/semi-icons';
-import styles from './index.module.less';
+import './style.css';
 import Charts from './charts.jsx';
 
 const Frame = () => {
   return (
-    <div className={styles.rootSidenav}>
+    <div className="rootSidenav">
       <Nav
         mode="horizontal"
         header={{
           text: 'ChartHub Templates'
         }}
         footer={
-          <div className={styles.dIV}>
-            <IconFeishuLogo size="large" className={styles.semiIconsFeishuLogo} />
-            <IconHelpCircle size="large" className={styles.semiIconsHelpCircle} />
-            <IconBell size="large" className={styles.semiIconsBell} />
+          <div className="dIV">
+            <IconFeishuLogo size="large" className="semiIconsFeishuLogo" />
+            <IconHelpCircle size="large" className="semiIconsHelpCircle" />
+            <IconBell size="large" className="semiIconsBell" />
             <Avatar
               size="small"
               src="https://sf6-cdn-tos.douyinstatic.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/avatarDemo.jpeg"
               color="blue"
-              className={styles.avatar}
+              className="avatar"
             >
               示例
             </Avatar>
           </div>
         }
-        className={styles.nav}
+        className="nav"
       ></Nav>
-      <div className={styles.main}>
-        <div className={styles.right}>
-          <div className={styles.frame1321317607}>
+      <div className="main">
+        <div className="right">
+          <div className="frame1321317607">
             <Charts />
           </div>
         </div>

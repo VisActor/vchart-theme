4d91b2d8fd37cd99163e7f9d24142a27e172aaaa
xiaoluoHe <sevnape@outlook.com>
2024-06-11 15:41:04 +0800

    chore: support less compile

--- packages/vchart-theme-demo-component/src/vchart-chart-hub-theme/frame.tsx
+++ packages/vchart-theme-demo-component/src/vchart-chart-hub-theme/frame.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, Avatar } from '@douyinfe/semi-ui';
 import { IconFeishuLogo, IconHelpCircle, IconBell } from '@douyinfe/semi-icons';
-import './style.css';
+import './style/style';
 import Charts from './charts.jsx';
 
 const Frame = () => {

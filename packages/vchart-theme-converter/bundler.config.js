1709317ecc3eb750506a1a4d76cb3673c496473c
xiaoluoHe <sevnape@outlook.com>
2024-07-24 17:06:06 +0800

    chore: update bundle config & package.json

--- packages/vchart-theme-converter/bundler.config.js
+++ packages/vchart-theme-converter/bundler.config.js
@@ -1,7 +1,13 @@
-
 /**
  * @type {Partial<import('@dp/bundler').Config>}
  */
 module.exports = {
-  formats: ["cjs", "es", "umd"],
+  formats: ['cjs', 'es', 'umd'],
+  outputDir: {
+    es: 'esm',
+    cjs: 'cjs',
+    umd: 'build'
+  },
+  name: 'VChartThemeConverter',
+  umdOutputFilename: 'index'
 };
